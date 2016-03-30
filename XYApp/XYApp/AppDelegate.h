e46d883c7a95df381356260b58abea3adfcc899d
Raymond <83002783@qq.com>
2016-03-30 20:05:56 +0800

    创建工程

---
+++ XYApp/XYApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XYApp
+//
+//  Created by lirui on 16/3/30.
+//  Copyright © 2016年 lirui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XYApp/XYApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XYApp
+//
+//  Created by lirui on 16/3/30.
+//  Copyright © 2016年 lirui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
